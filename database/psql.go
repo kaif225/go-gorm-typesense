bdd920edc3f419466765e41a0ecf6040c7a48eed
kaif225 <100071569+kaif225@users.noreply.github.com>
2025-11-16 03:18:14 +0530

    some -change

--- database/psql.go
+++ database/psql.go
@@ -19,20 +19,20 @@ func Connect() error {
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
 	port := os.Getenv("DB_PORT")
-	sslmode := os.Getenv("DB_SSLMODE")   // optional
-	timezone := os.Getenv("DB_TIMEZONE") // optional
+	// sslmode := os.Getenv("DB_SSLMODE")   // optional
+	// timezone := os.Getenv("DB_TIMEZONE") // optional
 
-	// fallback defaults (optional)
-	if sslmode == "" {
-		sslmode = "disable"
-	}
-	if timezone == "" {
-		timezone = "Asia/Kolkata"
-	}
+	// // fallback defaults (optional)
+	// if sslmode == "" {
+	// 	sslmode = "disable"
+	// }
+	// if timezone == "" {
+	// 	timezone = "Asia/Kolkata"
+	// }
 
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
-		host, user, password, dbname, port, sslmode, timezone,
+		"host=%s user=%s password=%s dbname=%s port=%s",
+		host, user, password, dbname, port,
 	)
 
 	//dsn := "host=localhost user=postgres password=example dbname=store port=5432 sslmode=disable TimeZone=Asia/Kolkata"
