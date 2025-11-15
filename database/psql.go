c05d250883f858ce0da57d365fcfc5be50dd31b9
kaif225 <100071569+kaif225@users.noreply.github.com>
2025-11-16 02:43:50 +0530

    some -change

--- database/psql.go
+++ database/psql.go
@@ -1,7 +1,9 @@
 package database
 
 import (
+	"fmt"
 	"log"
+	"os"
 	"psql-typesense/models"
 
 	"gorm.io/driver/postgres"
@@ -12,7 +14,28 @@ var DB *gorm.DB
 
 func Connect() error {
 
-	dsn := "host=localhost user=postgres password=example dbname=store port=5432 sslmode=disable TimeZone=Asia/Kolkata"
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+	sslmode := os.Getenv("DB_SSLMODE")   // optional
+	timezone := os.Getenv("DB_TIMEZONE") // optional
+
+	// fallback defaults (optional)
+	if sslmode == "" {
+		sslmode = "disable"
+	}
+	if timezone == "" {
+		timezone = "Asia/Kolkata"
+	}
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
+		host, user, password, dbname, port, sslmode, timezone,
+	)
+
+	//dsn := "host=localhost user=postgres password=example dbname=store port=5432 sslmode=disable TimeZone=Asia/Kolkata"
 	var err error
 
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
